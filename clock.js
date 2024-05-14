e5fddd5ff73a34dc716c68bc7a0dcf35ff812a71
VidhunKr <vidhunkrishna007@gmail.com>
2024-05-14 13:17:53 +0530

    first commit

---
+++ clock.js
@@ -0,0 +1,40 @@
+const hours=document.querySelector("#hours")
+const mintutes=document.querySelector("#minutes")
+const seconds=document.querySelector("#seconds")
+const section=document.querySelector("#section")
+
+const btn=document.querySelector("#btn")
+btn,addEventListener("click",()=>{
+    if(btn.innerHTML==="Dark"){
+        btn.innerHTML="Light"
+        document.body.style.backgroundColor="black"
+        document.body.style.color="#00ffff"
+    }else{
+        btn.innerHTML="Dark"
+        document.body.style.backgroundColor="white"
+        document.body.style.color="#black"
+
+    }
+})
+
+const DisplayTime=()=>{
+    let hrs=new Date().getHours()
+    let min=new Date().getMinutes()
+    let sec=new Date().getSeconds()
+    if (hrs>=12){
+        section.innerHTML="PM"
+    }else{
+        section.innerHTML="Am"
+    }
+    if(hrs>12){
+        hrs=hrs-12
+    }if(min<10){
+        min="0"+min
+    }if(sec<10){
+        sec="0"+sec
+    }
+    hours.innerHTML=hrs
+    mintutes.innerHTML=min
+    seconds.innerHTML=sec
+}
+setInterval(DisplayTime,10)
